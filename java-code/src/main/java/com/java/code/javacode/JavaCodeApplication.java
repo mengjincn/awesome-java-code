873b410e21d944fb8a3f741325617a4e62d21f9f
mengjin <mengjinb@yonyou.com>
2017-07-05 15:07:43 +0800

    add java-code project

---
+++ java-code/src/main/java/com/java/code/javacode/JavaCodeApplication.java
@@ -0,0 +1,12 @@
+package com.java.code.javacode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCodeApplication.class, args);
+	}
+}
