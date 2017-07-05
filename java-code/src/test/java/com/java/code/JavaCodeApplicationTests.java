4bfb1d108222c8183022978af39ba92e1d112241
mengjin <mengjinb@yonyou.com>
2017-07-05 18:20:50 +0800

    add jdbc template test

--- java-code/src/test/java/com/java/code/JavaCodeApplicationTests.java
+++ java-code/src/test/java/com/java/code/JavaCodeApplicationTests.java
@@ -1,16 +1,29 @@
 package com.java.code;
 
+import static org.hamcrest.Matchers.greaterThan;
+import static org.junit.Assert.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JavaCodeApplicationTests {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testJdbcTemplate() {
+		Integer count = jdbcTemplate.queryForObject("select count(pk) from operator", Integer.class);
+		assertThat(count, greaterThan(0));
 	}
 
 }
