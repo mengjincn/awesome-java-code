9560220846450616f9578dabdef1a675e702a690
mengjin <mengjinb@yonyou.com>
2017-07-05 17:10:26 +0800

    add baseencoding test

--- java-code/src/test/java/com/java/code/SimpleJavaCodeTest.java
+++ java-code/src/test/java/com/java/code/SimpleJavaCodeTest.java
@@ -5,6 +5,8 @@ import org.junit.Rule;
 
 import org.springframework.boot.test.rule.OutputCapture;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.BaseEncoding;
 import com.java.code.util.MathUtils;
 
 import static org.hamcrest.Matchers.*;
@@ -18,6 +20,7 @@ public class SimpleJavaCodeTest {
 		System.out.println("Hello World!");
 		assertThat(capture.toString(), containsString("World"));
 	}
+	
 	@Test
 	public void testIsPrime(){
 		assertTrue(MathUtils.isPrime(5));
@@ -26,5 +29,14 @@ public class SimpleJavaCodeTest {
 	@Test
 	public void testSqrt(){
 		assertThat(MathUtils.sqrt(2), is(1.414213562373095));
+	}
+	@Test
+	public void testBaseEncoding(){
+		BaseEncoding baseEncoding = BaseEncoding.base64();
+		String hello = "Hello World";
+		String encodeHello = baseEncoding.encode(hello.getBytes(Charsets.UTF_8));
+		assertThat(encodeHello,equalTo("SGVsbG8gV29ybGQ="));
+		String decodeHello = new String(baseEncoding.decode(encodeHello),Charsets.UTF_8);
+		assertThat(decodeHello,equalTo(hello));
 	}
 }
