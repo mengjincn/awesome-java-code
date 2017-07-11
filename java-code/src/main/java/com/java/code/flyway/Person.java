b47f5947475cb038a954278ae76da9a716ddcd73
mengjin <mengjinb@yonyou.com>
2017-07-11 16:53:47 +0800

    add simple rabbitma sample

--- java-code/src/main/java/com/java/code/flyway/Person.java
+++ java-code/src/main/java/com/java/code/flyway/Person.java
@@ -16,12 +16,16 @@
 
 package com.java.code.flyway;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-public class Person {
+public class Person implements Serializable{
+
+	private static final long serialVersionUID = 2939344400490013140L;
 	@Id
 	@GeneratedValue
 	private Long id;
